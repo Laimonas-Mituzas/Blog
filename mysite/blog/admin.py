4134de7bcaa0a1cb65386040fc7db14546f2a106
Laimonas <laimonas.mituzas@gmail.com>
2026-01-31 00:57:32 +0200

    + admin modified, time zone settings changed

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created')
+    inlines = [CommentInline]
+    readonly_fields = ['created']
+    list_filter = ['author', 'created']
+    search_fields = ['title', 'content', 'author__']
+
+    fieldsets = [
+        ('Irasas ', {'fields': ('title', 'content', 'author', 'created')}),
+    ]
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
