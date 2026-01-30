ca79a070f5c26413f95e36caa308842374a45437
Laimonas <laimonas.mituzas@gmail.com>
2026-01-31 00:37:21 +0200

    + models, admin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField()
+    content = models.TextField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.content}"
