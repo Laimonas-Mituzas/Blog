67aecbadb7d19f369201adaf62b740012344d31b
Laimonas <laimonas.mituzas@gmail.com>
2026-02-06 23:38:19 +0200

    +pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
-
 from .models import Post
+from django.core.paginator import Paginator
 import jinja_filters
 
 
 # Create your views here.
 def posts(request):
+    posts = Post.objects.order_by('pk')
+    paginator = Paginator(posts, per_page=2)
+    page_number = request.GET.get('page')
+    paged_posts = paginator.get_page(page_number)
+
     context = {
-        'posts': Post.objects.order_by('pk'),
+        'posts': paged_posts,
     }
     return render(request, 'posts.html', context)
 
